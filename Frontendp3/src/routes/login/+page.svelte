eb6387a5bf6b907602ea9993b8b7dd65ea3b97e6
Mae Chee Delicana <mdelic22@student.aau.dk>
2025-10-31 13:14:42 +0100

    Organization forms

--- Frontendp3/src/routes/login/+page.svelte
+++ Frontendp3/src/routes/login/+page.svelte
@@ -1,3 +1,31 @@
-<h1>Organization Login</h1>
+<h1>Log ind</h1>
+
+<body>
+    <div class="main">
+        <form action="">
+            <label for="first">
+                Email:
+            </label>
+            <input type="text" id="email" name="email" required>
+
+            <label for="password">
+                Password:
+            </label>
+            <input type="password" id="password" name="password" required>
+
+            <div class="wrap">
+                <button type="Login">
+                    Log ind
+                </button>
+            </div>
+        </form>
+        
+        <p>Not registered?
+            <a href="#" style="text-decoration: none;">
+                Create an account
+            </a>
+        </p>
+    </div>
+</body>
 
 
