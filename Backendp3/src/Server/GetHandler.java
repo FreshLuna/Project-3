dc8b21ada5b98120cd6931f679dc8a7321031f8c
Luna <LunaWinther@proton.me>
2025-11-10 14:54:52 +0100

    added comments and put it in a folder
    wowiee

--- Backendp3/src/Server/GetHandler.java
+++ Backendp3/src/Server/GetHandler.java
@@ -1,9 +1,15 @@
+package Server;
+
 import com.sun.net.httpserver.HttpExchange;
 
 public class GetHandler {
     public String handle(HttpExchange exchange) {
+
+        // we retrieve the full path the user attempts to access
         String path = exchange.getRequestURI().getPath();
 
+        // switch handles the different possible paths, write full path for now
+        // since im to lazy to cut off the server part mwuhahahhaha!!! >:3
         return switch (path) {
             case "/server/users" -> "List of users";
             case "/server/status" -> "Server is running";
