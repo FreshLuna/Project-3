8554af6b484872abc9689c25dd4dd4bc534f1683
lshayes <lhayes24@student.aau.dk>
2025-11-27 10:01:55 +0100

    feat: add Published
    Added publish method for activities

--- Backendp3/src/main/java/Server/PostHandler.java
+++ Backendp3/src/main/java/Server/PostHandler.java
@@ -1,22 +1,40 @@
 package Server;
 
+import Classes.Activity;
+import Events.Published;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import java.io.IOException;
 import Events.SignedUp;
 
 public class PostHandler {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
     public String handle(HttpExchange exchange) throws IOException {
         String path = exchange.getRequestURI().getPath(); // retrieves path
         String body = new String(exchange.getRequestBody().readAllBytes()); // retrieves the data send by the user
 
         return switch (path) {
             case "/server/echo" -> body;
+
             case "/server/participants" -> {
                 // Save posted participant data to participants.txt
                 SignedUp.appendParticipant(body);
                 yield "Participant saved";
             }
+
+            case "/server/publish" -> {
+
+                Activity activity = mapper.readValue(body, Activity.class);
+
+                Published.publish(activity);
+
+                yield "Activity published";
+            }
+
             case "/server/shutdown" -> "bye";
+
             default -> "Unknown POST path: " + path;
         };
     }
